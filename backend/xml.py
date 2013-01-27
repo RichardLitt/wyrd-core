200b41d8c5038381ad08c453f56bd31b03ac550e
Matěj Korvas <matej.korvas@gmail.com>
2013-01-27 21:03:54 +0100

    Allowed for alternatives of lxml.

--- backend/xml.py
+++ backend/xml.py
@@ -8,7 +8,32 @@ CC-Share Alike 2012 © The Wyrd In team
 https://github.com/WyrdIn
 
 """
-from lxml import etree
+try:
+  from lxml import etree
+  print("running with lxml.etree")
+except ImportError:
+  try:
+    # Python 2.5
+    import xml.etree.cElementTree as etree
+    print("running with cElementTree on Python 2.5+")
+  except ImportError:
+    try:
+      # Python 2.5
+      import xml.etree.ElementTree as etree
+      print("running with ElementTree on Python 2.5+")
+    except ImportError:
+      try:
+        # normal cElementTree install
+        import cElementTree as etree
+        print("running with cElementTree")
+      except ImportError:
+        try:
+          # normal ElementTree install
+          import elementtree.ElementTree as etree
+          print("running with ElementTree")
+        except ImportError:
+          print("Failed to import ElementTree from any known place")
+
 from datetime import datetime, timedelta
 import pytz
 
